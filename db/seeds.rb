8a51dd952cb18d82641dc6e786415684679c1e9a
kai-lee <bbruesch@mail.csuchico.edu>
2013-10-31 23:58:38 -0700

    finished seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+listing.create(name: 'Chrome', description: 'test12312test', url: 'http://www.chro32me.com', doi: '1')
+listing.create(name: 'Chroooome', description: 'tes31231ttest', url: 'http://www.chr121ome.com', doi: '2')
+listing.create(name: 'Chromeduh', description: 'test312312test', url: 'http://www.chr4121ome.com', doi: '3')
+listing.create(name: 'randomseed', description: 'tes3123123ttest', url: 'http://www.ch12312rome.com', doi: '4')
+listing.create(name: 'dataentry', description: 'tes1231321ttest', url: 'http://www.ch515125123rome.com', doi: '5')
+listing.create(name: 'noclue', description: 'testt3123132est', url: 'http://www.chro21321me.com', doi: '6')
+listing.create(name: 'keep', description: 'testte13231st', url: 'http://www.chr121ome.com', doi: '7')
+listing.create(name: 'it', description: 'test13212test', url: 'http://www.ch3123151rome.com', doi: '8')
+listing.create(name: 'Coming', description: 'test123123test', url: 'http://www.ch12rome.com', doi: '9')
+listing.create(name: 'now', description: 'test23121312test', url: 'http://www.chrome.com', doi: '10')
+ 
